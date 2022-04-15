ef680df5d449b50ae8c944aceaf2c8edbe10dbe9
Jennifer Carreno <jennifercarreno72@gmail.com>
2022-04-15 14:45:49 -0700

    Authors for posts are visible

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,13 +1,18 @@
 const Post = require('../models/post');
+const User = require('../models/user');
+const Comment = require('../models/comment');
 
 module.exports = (app) => {
 
   // home page displaying all posts
     app.get('/', async (req, res) => {
         try {
-          const posts = await Post.find({}).lean();
+          const posts = await Post.find({}).lean().populate('author')
           const currentUser = req.user;
-          return res.render('posts-index', { posts, currentUser });
+          const { user } = req;
+
+
+          return res.render('posts-index', { posts, user, currentUser });
         } catch (err) {
           console.log(err.message);
         }
@@ -23,13 +28,26 @@ module.exports = (app) => {
       // saves new post
       app.post('/posts/new', (req, res) => {
         if (req.user) {
+          const userId = req.user._id;
           const post = new Post(req.body);
-      
-          post.save(() => res.redirect('/'));
-        } else {
-          return res.status(401); // UNAUTHORIZED
-        }
-      });
+          post.author = userId;
+
+          post
+        .save()
+        .then(() => User.findById(userId))
+        .then((user) => {
+          user.posts.unshift(post);
+          user.save();
+          // REDIRECT TO THE NEW POST
+          return res.redirect(`/posts/${post._id}`);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    } else {
+      return res.status(401); // UNAUTHORIZED
+    }
+  });
 
   // displays one post
     app.get('/posts/:id', async (req, res) => {
